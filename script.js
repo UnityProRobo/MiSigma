f35511a96b528cd8af6ac2325d7adb4f57728216
UnityProRobo <166161883+UnityProRobo@users.noreply.github.com>
2024-04-05 20:50:55 +0300

    aboba

---
+++ script.js
@@ -0,0 +1,116 @@
+// let h1 = document.getElementById("h1");
+
+// let h2 = document.getElementById("h2");
+
+// let p = document.getElementById("p");
+
+// let li = document.getElementById("li");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let a = 5;
+// // let b = 10;
+// // let r = 20;
+// // let g = 40;
+// // let s = "stroka!";
+// // console.log(a+b);
+// // console.log("glutiky");
+
+// h1.addEventListener('click' ,
+// function(){
+//     alert("ошибка");
+
+    
+// });
+
+// h2.addEventListener('click' ,
+// function(){
+//     alert("pon");
+
+    
+// });
+// p.addEventListener('click' ,
+// function(){
+//     alert("ok");
+
+    
+// });
+// li.addEventListener('click' ,
+// function(){
+//     alert("liza");
+
+    
+// });
+// let sigma = prompt('Введите число 1');
+// let smileface = prompt('Введите число 1');
+// console.log();
+// alert( `
+// Cума чисел = 
+// ${
+
+//     Number(sigma)+
+//     Number(smileface)
+
+// }
+// `)
+
+
+
+
+
+
+
+
+
+let sigma = prompt('Ввдедите текст');
+let sigma3 = prompt('Введите число 1');
+let sigma2 = prompt('Введите число 2');
+
+
+if(sigma == "+"){
+    alert(`
+        Cума чисел =${ Number(sigma3)+Number(sigma2) 
+       
+    }
+`)}
+
+if(sigma == "-"){
+    alert(`
+        Cума чисел =${ Number(sigma3)-Number(sigma2) 
+       
+    }
+`)}
+if(sigma == "/"){
+    alert(`
+        Cума чисел =${ Number(sigma3)/Number(sigma2) 
+       
+    }
+`)}
+if(sigma == "*"){
+    alert(`
+        Cума чисел =${ Number(sigma3)*Number(sigma2) 
+       
+    }
+`)}
+
+
